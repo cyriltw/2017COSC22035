a1352fb85410429bf8b1b99bb45b0c96f5418040
Cyril Mac <thari69@gmail.com>
2017-12-12 16:10:00 +0530

    GUI

--- src/Login.java
+++ src/Login.java
@@ -1,17 +1,66 @@
+import jdk.nashorn.internal.scripts.JO;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+import DBConnect.*;
 
 public class Login {
     private JPanel panel1;
     private JTextField txtUsername;
     private JPasswordField txtPW;
-    private JButton exitButton;
-    private JButton loginButton;
+    private JButton btnExit;
+    private JButton btnLogin;
     public static JFrame logFR = new JFrame("Login for FIS");
+    private Statement stml=null;
+
+    public Login() {
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!validateUserPW())JOptionPane.showMessageDialog(null,"Sorry, "+txtUsername.getText()+" Login Failed");
+                else
+                {
+                    JOptionPane.showMessageDialog(null,"Login Successful");
+                    new UserDetails(txtUsername.getText()).setVisible(true);
+                    logFR.setVisible(false);
+                }
+            }
+        });
+    }
 
     public static void main(String[] args) {
         logFR.setContentPane(new Login().panel1);
         logFR.pack();
         logFR.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         logFR.setVisible(true);
+    }
+
+    private boolean validateUserPW()
+    {
+        String uName = txtUsername.getText();
+        String PW = String.valueOf(txtPW.getPassword());
+        DBConnect.dbConnect DB = new dbConnect();//Object from DB connect
+        stml = DB.Conn2DB();//Connect to DB
+        String sql = "Select * from userdetails where userName='"+uName+"'";
+        try{
+            ResultSet rs = stml.executeQuery(sql);
+            rs.first();
+            String dbPW = rs.getString("Password");
+            if (PW.equals(dbPW))
+                return true;
+        }catch (SQLException se)
+        {
+            se.printStackTrace();
+        }
+        return false;
     }
 }
---
+++ src/UserDetails.java
@@ -0,0 +1,49 @@
+import DBConnect.dbConnect;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class UserDetails extends JFrame {
+    private JPanel panel1;
+    private JTextField txtUsername;
+    private JTextField textField1;
+    private JTextField txtEmail;
+    private JTextField txtNumber;
+    private JTextField txtGPA;
+    private JTextField txtAcdYr;
+    private JButton exitButton;
+    private JButton coursesButton;
+    private Statement stml=null;
+
+    public UserDetails(String userName) {
+        setContentPane(panel1);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        fillDetails(userName);
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+    }
+    private void fillDetails(String userName)
+    {
+        DBConnect.dbConnect DB = new dbConnect();//Object from DB connect
+        stml = DB.Conn2DB();//Connect to DB
+        String query = "Select * from userDetails where UserName='"+userName+"'";
+        try{
+            ResultSet rs= stml.executeQuery(query);
+            rs.first();
+            txtUsername.setText(rs.getString("UserName"));
+            txtEmail.setText(rs.getString("Email"));
+        }catch (SQLException se)
+        {
+            se.printStackTrace();
+        }
+    }
+}
